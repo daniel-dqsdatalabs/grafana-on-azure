6b24d4aa36fa23cdf22b04a9e7efafd31f349d5d
Daniel <68124272+daniel-dqsdatalabs@users.noreply.github.com>
2020-08-20 07:42:07 -0300

    Create drop.sh

---
+++ drop.sh
@@ -0,0 +1,13 @@
+# Defining variables
+resource_group_name="dqsdatalabs-grafana"
+
+# Removing Resources
+az group delete --name $resource_group_name
+
+# Removing AD App
+ad_application_id=$(az ad app list \
+    --display-name Grafana \
+    --query '[0].appId' \
+    --output tsv)
+
+az ad app delete --id $ad_application_id
